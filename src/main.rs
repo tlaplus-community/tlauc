65c9713091898bd1abb1dbb2cb0a5cc3e5e18a56
Andrew Helwer <ahelwer@protonmail.com>
2021-08-25 11:14:49 -0700

    Command line parameter parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
 use tree_sitter::{Parser};
+use std::fs::File;
+use std::fs::OpenOptions;
 
-fn main() {
+fn to_unicode(spec : File, ignore_errors : bool) {
     let mut parser = Parser::new();
     parser.set_language(tree_sitter_tlaplus::language()).expect("Error loading TLA+ grammar");
     let source_code = r#"
@@ -9,5 +13,62 @@ fn main() {
         ===="#;
     let tree = parser.parse(source_code, None).unwrap();
     println!("{}", tree.root_node().to_sexp());
+}
+
+fn to_ascii(spec : File, ignore_errors : bool) {
+
+}
+
+fn main() {
+    let matches =
+        App::new("TLA+ Unicode Converter")
+            .version("0.1.0")
+            .author("Andrew Helwer <ahelwer@protonmail.com>")
+            .about("Converts symbols in TLA+ specs to and from unicode")
+            .arg(Arg::with_name("out_file")
+                .short("o")
+                .long("out-file")
+                .help("Output file; rewrites input file if not given")
+                .takes_value(true)
+                .required(false)
+            )
+            .arg(Arg::with_name("ignore_parse_errors")
+                .short("e")
+                .long("ignore-error")
+                .help("Whether to convert file despite parse errors")
+                .required(false)
+            )
+            .subcommand(SubCommand::with_name("to")
+                .about("Converts symbols in TLA+ spec to unicode from ASCII")
+                .arg(Arg::with_name("spec")
+                    .help("The TLA+ spec file to convert")
+                    .required(true)
+                    .index(1)
+                )
+            ).subcommand(SubCommand::with_name("from")
+                .about("Converts symbols in TLA+ spec from unicode to ASCII")
+                .arg(Arg::with_name("spec")
+                    .help("The TLA+ spec file to convert")
+                    .required(true)
+                    .index(1)
+                )
+            ).get_matches();
+
+    let ignore_errors = matches.is_present("ignore_parse_errors");
+    if let Some(matches) = matches.subcommand_matches("to") {
+        let file_path_str = matches.value_of("spec").unwrap();
+        match OpenOptions::new().read(true).write(true).open(file_path_str) {
+            Ok(spec) => to_unicode(spec, ignore_errors),
+            Err(e) => println!("Error opening input file [{}]: [{}]", file_path_str, e)
+        }
+    } else if let Some(matches) = matches.subcommand_matches("from") {
+        let file_path_str = matches.value_of("spec").unwrap();
+        match OpenOptions::new().read(true).write(true).open(file_path_str) {
+            Ok(spec) => to_ascii(spec, ignore_errors),
+            Err(e) => println!("Error opening input file [{}]: [{}]", file_path_str, e)
+        }
+    } else {
+        println!("{}", matches.usage());
+    }
 }
 
